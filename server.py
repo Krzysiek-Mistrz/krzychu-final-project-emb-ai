c0f8d72c35e4cfc9524f19569c7fcc1c4291cc5b
Krzysztof <krzyskuar@gmail.com>
2025-02-06 23:58:41 +0100

    modified code to be PEP compliant

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
-from flask import Flask, request, jsonify, render_template
+"""
+Creates flask server to handle requests to app which utilizes NN of watson.ai
+
+Functions:
+
+sent_analyzer
+render_index_page
+"""
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detection
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector", methods=["GET"])
 def sent_analyzer():
+    """
+    Function sent_analyzer takes arguments from js file and send 
+    them to emotion_detection which actually sends them to NN, 
+    which performs NLP.
+
+    Parameters:
+        None
+
+    Returns:
+        string: analyzed text with emotions statistic
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
     if not text_to_analyze:
         return "Invalid text! Please try again!"
 
@@ -19,15 +38,24 @@ def sent_analyzer():
         "joy": response.get("joy"),
         "sadness": response.get("sadness"),
     }
-    
     dominant_emotion = response.get('dominant_emotion')
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-
-    return f"For the given statement, the system response is {result}. The dominant emotion is {dominant_emotion}."
+    fstr1 = f"For the given statement, the system response is {result}. "
+    fstr2 = f"The dominant emotion is {dominant_emotion}."
+    return fstr1 + fstr2
 
 @app.route("/")
 def render_index_page():
+    """
+    Function which actually only renders index.html on start of app
+
+    Parameters:
+        None
+
+    Returns:
+        <file>.html
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
